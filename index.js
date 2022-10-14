f6ce22bccf4a23ed623afc5d8c7d75b0fc48d415
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2022-10-14 00:27:02 -0500

    commit 5

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = CyclicDB("wild-plum-bunny-togaCyclicDB")
 
 const animals = db.collection("animals")
 
-async function animals(){
+async function test('should first', () => { second })(){
     // create an item in collection with key "leo"
     let leo = await animals.set("leo", {
         type: "cat",
