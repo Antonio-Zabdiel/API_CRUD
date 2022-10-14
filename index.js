4450cd5c875bafd447054fc36e5dc0e91b2031c8
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2022-10-14 00:10:57 -0500

    first commit2

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+var app = express();
+
+const PORT = process.env.PORT;
+
+//database
+const CyclicDB = require("cyclic-dynamodb")
+const db = CyclicDB("wild-plum-bunny-togaCyclicDB")
+
+
+const animals = db.collection("animals")
+
+// create an item in collection with key "leo"
+let leo = await animals.set("leo", {
+    type: "cat",
+    color: "orange"
+})
+
+// get an item at key "leo" from collection animals
+let item = await animals.get("leo")
+console.log(item)
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
